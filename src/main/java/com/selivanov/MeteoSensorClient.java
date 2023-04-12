b541b870101161b115da6ccb2565757df03ed102
TimSelivan <timselivanson@gmail.com>
2023-04-12 11:40:53 +0300

    Implement makePostRequestWithJsonData method

--- src/main/java/com/selivanov/MeteoSensorClient.java
+++ src/main/java/com/selivanov/MeteoSensorClient.java
@@ -1,9 +1,32 @@
 package com.selivanov;
 
-public class MeteoSensorClient
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+public class MeteoSensorClient {
+    public static void main(String[] args) {
+
+        final String sensorName = "Test Sensor";
+    }
+
+    private static void makePostRequestWithJsonData(String url, Map<String, Object> jsonData) {
+        final RestTemplate restTemplate = new RestTemplate();
+
+        final HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<Object> request = new HttpEntity<Object>(jsonData, headers);
+
+        try {
+            restTemplate.patchForObject(url, request, String.class);
+        } catch (HttpClientErrorException exception) {
+            System.out.println("Error");
+            System.out.println(exception.getMessage());
+        }
     }
 }
